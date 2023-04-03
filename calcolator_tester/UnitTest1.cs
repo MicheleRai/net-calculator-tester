db7bd7dd6e6bcf12da8a46ca0e873498f059577d
MicheleRai <rainonemichele1999@gmail.com>
2023-04-03 14:39:11 +0200

    Add project files.

---
+++ calcolator_tester/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace calcolator_tester
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
