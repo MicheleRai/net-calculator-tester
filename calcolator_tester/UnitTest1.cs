cb66148b381816af54a182cd7347b89a53fc7806
MicheleRai <rainonemichele1999@gmail.com>
2023-04-03 15:42:42 +0200

    all metod added to test

--- calcolator_tester/UnitTest1.cs
+++ calcolator_tester/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Calculator;
 using NUnit.Framework;
 
 namespace calcolator_tester
@@ -9,11 +10,51 @@ namespace calcolator_tester
         public void Setup()
         {
         }
+        [Test]
+        [TestCase(10, 5, 15)]
+        [TestCase(-10, -5, -15)]
+        [TestCase(-10, 5, -5)]
+        [TestCase(10, -5, 5)]
+        [TestCase(0, 5, 5)]
+        [TestCase(0, 0, 0)]
+        public void Add(float a, float b, float c)
+        {
+            Assert.That(Calculator.Add(a, b), Is.EqualTo(c));
+        }
 
         [Test]
-        public void Test1()
+        [TestCase(10, 5, 5)]
+        [TestCase(-10, -5, -5)]
+        [TestCase(-10, 5, -15)]
+        [TestCase(10, -5, 15)]
+        [TestCase(0, 5, -5)]
+        [TestCase(0, 0, 0)]
+        public void Subtract(float a, float b, float c)
         {
-            Assert.Pass();
+            Assert.That(Calculator.Subtract(a, b), Is.EqualTo(c));
+        }
+
+        [Test]
+        [TestCase(10, 2, 5)]
+        [TestCase(-10, 2, -5)]
+        [TestCase(10, -2, -5)]
+        [TestCase(-10, -2, 5)]
+        public void Divide(float a, float b, float c)
+        {
+            Assert.That(Calculator.Divide(a, b), Is.EqualTo(c));
+        }
+
+        [Test]
+        [TestCase(10, 5, 20)]
+        [TestCase(10, -5, -50)]
+        [TestCase(-10, 5, -50)]
+        [TestCase(-10, -5, 50)]
+        [TestCase(10, 0, 0)]
+        [TestCase(0, 10, 0)]
+        [TestCase(0, 0, 0)]
+        public void Multiply(float a, float b, float c)
+        {
+            Assert.That(Calculator.Multiply(a, b), Is.EqualTo(c));
         }
     }
 }
--- net-calculator-tester/Calculator.cs
+++ net-calculator-tester/Calculator.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public static class Calculator
+namespace Calculator
 {
-    public static float Add(float x, float y) { return x + y; }
+    public static class Calc
+    {
+        public static float Add(float x, float y) { return x + y; }
 
-    public static float Subtract(float x, float y) { return x - y; }
+        public static float Subtract(float x, float y) { return x - y; }
 
-    public static float Divide(float x, float y) { return x / y; }
+        public static float Divide(float x, float y) 
+        {
+            if (x == 0 || y == 0) throw new ArgumentException("usare zero come dividendo o divisore non è possibile");
+            return x / y;
+        }
 
-    public static float Multiply(float x, float y) { return x * y; }
+        public static float Multiply(float x, float y) { return x * y; }
+    }
 }
+
+
